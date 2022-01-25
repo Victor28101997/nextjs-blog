086a135e227a2cb429d03c5180a2bc382e6e1859
Thang. Pham Duc - CMCGlobal DU11 <thangpham.cmcglobal@gmail.com>
2022-01-25 22:42:18 +0700

    tweak handler

--- handler.js
+++ handler.js
@@ -1,8 +1,12 @@
 const baseHandler = require("./index.js");
 
 export const onOriginEventReceived = async (event, context) => {
-  const { request } = event.Records[0].cf;
-
-  request.uri = "/serverside";
-  return baseHandler.handler(event);
+  try {
+    const { request } = event.Records[0].cf;
+    request.uri = "/serverside";
+    return baseHandler.handler(event);
+  } catch (error) {
+    request.uri = "/serverside";
+    return baseHandler.handler(event);
+  }
 };
