a6cb0a8f2f93021e4950a8360985b576142f3218
Thang. Pham Duc - CMCGlobal DU11 <thangpham.cmcglobal@gmail.com>
2022-01-26 12:53:59 +0700

    adjust _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import "../styles/global.css";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    console.log("_app mounted!!");
+  }, []);
   return <Component {...pageProps} />;
 }
